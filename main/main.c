1a8bf14282fa0c089ea759a3d7594651f38ae945
Oleg Skvortsov <highsoft.soi@gmail.com>
2026-05-02 12:32:20 -0700

    Added WiFi connection logic

--- main/main.c
+++ main/main.c
@@ -2,6 +2,8 @@
 #include "esp_camera.h"
 #include "freertos/FreeRTOS.h"
 #include "esp_log.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
 
 #define CAM_PIN_PWDN    32
 #define CAM_PIN_RESET   -1
@@ -22,7 +24,56 @@
 #define CAM_PIN_HREF    23
 #define CAM_PIN_PCLK    22
 
+#define WIFI_SSID "Soi13"
+#define WIFI_PASS ""
+
 static const char *TAG = "ESP32-CAM";
+
+// Wifi event handler for displaying parameters of connection
+static void event_handler(void* arg, esp_event_base_t event_base,
+                          int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        wifi_event_sta_disconnected_t *event = (wifi_event_sta_disconnected_t *)event_data;
+        ESP_LOGW(TAG, "Wi-Fi disconnected, retrying to reconnect. The reason is %d ", event->reason);
+        esp_wifi_connect();
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "Wi-Fi connected");
+        ESP_LOGI(TAG, "IP Address: " IPSTR, IP2STR(&event->ip_info.ip));
+        ESP_LOGI(TAG, "Subnet Mask: " IPSTR, IP2STR(&event->ip_info.netmask));
+        ESP_LOGI(TAG, "Gateway: " IPSTR, IP2STR(&event->ip_info.gw));
+    }
+}
+
+// Initializing Wifi connection
+static void wifi_init(void)
+{
+    esp_netif_init();
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL);
+    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL);
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_set_max_tx_power(40)); //This method specifically for ESP32-C3, otherwise it will not connect to WiFi.
+}
 
 //Camera initialization
 static esp_err_t camera_init(void)
@@ -87,9 +138,13 @@ void capture_photo(void)
 void app_main(void)
 {
     if (camera_init() != ESP_OK) {
-        printf("Camera init failed\n");
+        ESP_LOGE(TAG, "Camera init failed!");
         return;
     }
+
+    //ESP_ERROR_CHECK(nvs_flash_init());
+    //wifi_init();
+    //vTaskDelay(pdMS_TO_TICKS(5000));
 
     //printf("Camera initialized\n");
     ESP_LOGI(TAG, "Camera initialized");
